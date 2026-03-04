7008a65c3b21016dce292f10e8f20453006a0ae9
ilias195 <iliasafc@outlook.com>
2026-03-04 22:21:40 +0100

    healthpower up werkt nog niet als je als player de heart land player word destroyed

--- Assets/Scripts/Platformer/PowerUps/HealPowerUp.cs
+++ Assets/Scripts/Platformer/PowerUps/HealPowerUp.cs
@@ -1,17 +1,26 @@
+
 using UnityEngine;
 
 public class HealPowerUp : MonoBehaviour
 {
     [SerializeField] private int _healAmount = 25;
+    [SerializeField] private AudioClip _healSound;   
+    [SerializeField] private float _volume = 1f;     
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-         PlayerHealth player = other.GetComponent<PlayerHealth>();
+        PlayerHealth player = other.GetComponent<PlayerHealth>();
+
 
         if (player != null)
         {
+            Debug.Log("Heal picked up");
             player.Heal(_healAmount);
-            Destroy(gameObject);
+
+            //  speel geluid af op positie van object
+            AudioSource.PlayClipAtPoint(_healSound, transform.position, _volume);
+
+            Destroy(other.gameObject);
         }
     }
 }
